3db803b3bc0d75a65628f7afff789b6170bf1607
mroley1 <64291507+mroley1@users.noreply.github.com>
2023-05-18 22:32:27 -0400

    add education card

--- main.js
+++ main.js
@@ -13,3 +13,19 @@ for (i = 0; i < socialButtons.length; i++) {
         e.target.src = e.target.src.replace(/selected/, "default");
     });
 }
+
+let dividers = document.querySelectorAll(".divider")
+for (i = 0; i < dividers.length; i++) {
+    let divider = dividers[i];
+    var context = divider.getContext("2d");
+    var width = divider.clientWidth;
+    const dividerIndex = parseInt(divider.getAttribute("data-divider-after"))
+    context.fillStyle = getComputedStyle(document.body).getPropertyValue("--main-colors-"+dividerIndex);
+    context.fillRect(0, 0, 1000, 100);
+    context.fillStyle = getComputedStyle(document.body).getPropertyValue("--main-colors-"+(dividerIndex+1));
+    context.beginPath();
+    context.moveTo(0, 100);
+    context.lineTo(500, 0);
+    context.lineTo(1000,100);
+    context.fill();
+}
